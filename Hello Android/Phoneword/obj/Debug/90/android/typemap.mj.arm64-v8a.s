7068aef73c309f6d19e30c337339a0b3881c844e
Christian Bedwell <Christian.8edwell@gmail.com>
2019-10-28 12:19:37 -0400

    Added Xamarin tutorial which translates an alphanumeric phone number to numeric.

---
+++ Hello Android/Phoneword/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 60d52b2851e5190fc8257a39cda8684e9e7d08db */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1147
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300515
+	.include	"typemap.mj.inc"
---
+++ Hello Android/Phoneword/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 60d52b2851e5190fc8257a39cda8684e9e7d08db */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1147
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300515
+	.include	"typemap.mj.inc"
